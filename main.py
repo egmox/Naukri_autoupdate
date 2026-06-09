d0ec98f7b684659281abf4907c4764563878d0b7
unknown <egmox2@gmail.com>
2026-06-09 10:41:26 +0530

    added req in handler in main

--- main.py
+++ main.py
@@ -506,7 +506,7 @@ def get_all_accounts():
     return accounts
 
 
-def handler():
+def handler(req):
     log_msg("-----Naukri.py Script Run Begin-----")
     accounts = get_all_accounts()
     if not accounts:
